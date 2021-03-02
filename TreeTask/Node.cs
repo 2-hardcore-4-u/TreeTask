651cdd5b31b7b5352a45dd9e00b4e5322866d1f9
Nikita Smolyaninov <nikita.smolaninov@gmail.com>
2021-03-02 22:22:37 +0200

    Second commit

---
+++ TreeTask/Node.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TreeTask
+{
+    class Node<T>
+    {
+        public T value;
+        public Node<T> left { get; set; }
+        public Node<T> right { get; set; }
+
+        public Node(T v)
+        {
+            this.value = v;
+        }
+        public Node()
+        {
+            this.value = default;
+        }
+    }
+}
